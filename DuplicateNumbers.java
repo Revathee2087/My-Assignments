97378f28fcb619b1a307b132bc08b8e22350c67d
Revathee2087 <139657001+Revathee2087@users.noreply.github.com>
2023-07-17 22:42:31 +0530

    Add files via upload

---
+++ DuplicateNumbers.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class DuplicateNumbers {
+
+	public static void main(String[] args) {
+		int[] nums={2,5,7,7,5,9,2,3};
+		
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length-1; i++) {
+			if(nums[i]==nums[i+1]) {
+				System.out.println("Duplicate numbers is:"+nums[i+1]);
+	}
+		}
+
+}
+
+}
